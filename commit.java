5c02176caadfd4ba94d1ebec1f201ae35c504b85
unknown <elliejeong95@gmail.com>
2020-02-19 01:35:23 -0500

    useful git commands

--- commit.java
+++ commit.java
@@ -1,9 +1,16 @@
 public class HelloWorld {
 
+/* git add <Filename>
+   git status -- to check if the file added correctly \
+   git commit -m 'Message'
+   git push  
+   git difftool HEAD --check the difference between current version and the previous version of the file
+   */
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
         System.out.println("Commit change");
+        
     }
 
 }
